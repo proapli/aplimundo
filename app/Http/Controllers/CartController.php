00e781c9d0818db6ef5adda656e381c537c744bf
proapli <proapli@hotmail.com>
2019-02-25 19:46:00 -0600

    Change app name and add search

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use App\User;
+use App\Mail\NewOrder;
+use Mail;
 
 class CartController extends Controller
 {
-    
-      public function update(){
-            
-            $cart = auth()->user()->cart;
-            $cart->status = 'Pending';
-            $cart ->save(); //UPDATE 
-            
-            $notification = 'Tu registro se ha registrado correctamente. Te contactaremos via mail';
-            return back()->with(compact('notification'));
+    public function update()
+    {
+    	$client = auth()->user(); 
+    	$cart = $client->cart;
+    	$cart->status = 'Pending';
+    	$cart->order_date = Carbon::now();
+    	$cart->save(); // UPDATE
 
-      }
+    	$admins = User::where('admin', true)->get();
+    	Mail::to($admins)->send(new NewOrder($client, $cart));
 
+    	$notification = 'Tu pedido se ha registrado correctamente. Te contactaremos pronto vía mail!';
+    	return back()->with(compact('notification'));
+    }
 }
--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hola@pedidos.icu'),
+        'name' => env('MAIL_FROM_NAME', 'App Shop'),
     ],
 
     /*
